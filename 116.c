ee181bf1872c8867304494a2cbb0b760860fe4f0
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-07 16:02:58 +0530

    Create 116.c

---
+++ 116.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,print;
+    printf("enter the two value n and k");
+    scanf("%d %d",&n , &k);
+    if(n==0||k==0)
+    print=n+k;
+    printf("the value of two number add the %d",print);
+}
