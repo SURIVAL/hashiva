8434e4b2f2d46331aac192a339694b0d3fd8ea67
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-12 15:35:53 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the a and b value");
+scanf("%d%d",&a,&b);
+c=a+b;
+if(a==b)
+{
+printf("it is a perfect square");
+}
+else
+{
+printf("it is not a perfect square");
+}
+}
