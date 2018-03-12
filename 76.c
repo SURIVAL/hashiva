0a7ab5d3426390dfbe04643c00d1fbeb0bde17e8
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-12 15:42:03 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int n,i,flag=0;
+printf("enter the n value");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+    if(n%i==0)
+    {
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("\n no");
+}
+else
+{
+printf("\n yes");
+}
+return 0;
+}
