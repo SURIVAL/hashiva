8e221dafd4a1b16faabd47b4a001169856f26168
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-12 15:48:19 +0530

    Create 77.c

---
+++ 77.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	int num,i;
+	printf("enter the number:");
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			printf("%d ",i);
+		}
+	}
+	return 0;
+}
